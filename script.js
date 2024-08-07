99a373345465faf6719030f05297444bce901147
AldrichKenneth <aldrichkenromano@gmail.com>
2024-08-07 18:11:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const myAudio = document.getElementById("august");
+const isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
